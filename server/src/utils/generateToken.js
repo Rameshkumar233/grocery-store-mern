ffc73343fb7f671d7f5c9fd6c51dcf9cf41062f4
Ramesh Kumar <test@youremail.com>
2026-05-26 16:27:19 +0530

    backend setup

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 export const generateToken = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1d" });
     res.cookie("jwt", token, { httpOnly: true, secure: isProduction, sameSite: isProduction ? "none" : "lax", maxAge: 24 * 60 * 60 * 1000 });
