2f913a7170c9692c7e0f259f2d4bd986e0515a0d
Ramesh Kumar <test@youremail.com>
2026-05-26 22:36:51 +0530

    update config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss()],
+    base: mode === "production" ? "/grocery-store-mern/" : "/",
 }));
